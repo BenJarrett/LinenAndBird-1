a3a148be9ed022261e046ba5cdc5e280bcfd7835
hmparsons-sde <holly-parsons@outlook.com>
2021-08-21 10:11:36 -0500

    in class work pt 1

--- LinenAndBird/Controllers/HatsController.cs
+++ LinenAndBird/Controllers/HatsController.cs
@@ -1,4 +1,5 @@
-﻿using LinenAndBird.Models;
+﻿using LinenAndBird.DataAccess;
+using LinenAndBird.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -9,50 +10,35 @@ using System.Threading.Tasks;
 namespace LinenAndBird.Controllers
 {
     [Route("api/hats")] //exposed at this endpoint
+    // ASP.NET is reading this metadata
     [ApiController] // an api controller, so it returns json or xml; This attribute indicates that the controller responds to web API requests. 
     public class HatsController : ControllerBase // A base class for an MVC controller without view support.
     {
-        static List<Hat> _hats = new List<Hat>
-            {
-                new Hat
-                {
-                    Color = "Blue",
-                    Designer = "Charlie",
-                    Style = HatStyle.OpenBack
-                },
-                new Hat
-                {
-                    Color = "Black",
-                    Designer = "Nathan",
-                    Style = HatStyle.WideBrim
-                },
-                new Hat
-                {
-                    Color = "Magenta",
-                    Designer = "Charlie",
-                    Style = HatStyle.Normal
-                }
-            };
-
+        HatRepository _repo; // field has type + name
+        public HatsController()
+        {
+            _repo = new HatRepository();
+        }
         [HttpGet] // attributes can be used to configure the behavior of web API controllers and action methods.
         public List<Hat> GetAllHats()
         {
-            return _hats;
+            var repo = new HatRepository();
+            return repo.GetAll();
         }
 
         // GET /api/hats/styles/1 -> all open backed hats
-        [HttpGet("styles/{style}")]
-        public IEnumerable<Hat> GetHatsByStyle(HatStyle style)
+        [HttpGet("styles/{style}")] // how should this method be exposed & executed
+        // all additive
+        public IEnumerable<Hat> GetHatsByStyle(HatStyle style) // IEnum is just to loop over; restrictive permissions
         {
-            var matches = _hats.Where(hat => hat.Style == style);
-
-            return matches;
+            return _repo.GetByStyle(style);
         }
 
         [HttpPost]
         public void AddAHat(Hat newHat)
         {
-            _hats.Add(newHat);
+            var repo = new HatRepository();
+            repo.Add(newHat);
         }
 
     }
