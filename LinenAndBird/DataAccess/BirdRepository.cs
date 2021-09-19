7511fc399aad9c4cca33e87f22aa4ac47a6cd422
hmparsons-sde <holly-parsons@outlook.com>
2021-09-19 14:47:28 -0500

    intro to Dapper; including example sql queries (create, update, delete) for posterity

--- LinenAndBird/DataAccess/BirdRepository.cs
+++ LinenAndBird/DataAccess/BirdRepository.cs
@@ -4,146 +4,213 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
+using Dapper;
 
 namespace LinenAndBird.DataAccess
 {
     public class BirdRepository
     {
-
+        // use 'Using Microsoft.Data.SqlClient;' up above first
+        // must have a connection to the database if you want to use ado.net:
+        // use connectionstrings.com/sql-server/ for documentation
         const string _connectionString = "Server=localhost;Database=LinenAndBird;Trusted_Connection=True;";
-
         internal IEnumerable<Bird> GetAll()
         {
-            //connections are like the tunnel between our app and the database
-            using var connection = new SqlConnection(_connectionString);
-            //connections aren't open by default, we've gotta do that ourself
-            connection.Open();
-
-            //this is what tells sql what we want to do
-            var command = connection.CreateCommand();
-            command.CommandText = @"Select *
-                                    From Birds";
-
-            //execute reader is for when we care about getting all the results of our query
-            var reader = command.ExecuteReader();
-
-            var birds = new List<Bird>();
-
-            //data readers are weird, only get one row from the results at a time
-            while(reader.Read())
-            {
-                var bird = MapFromReader(reader);
-
-                //each bird goes in the list to return later
-                birds.Add(bird);
-            }
+            // Dapper way:
+            using var db = new SqlConnection(_connectionString);
+            var birds = db.Query<Bird>(@"Select * From Birds");
 
             return birds;
+
+            // ADO - 
+            //connection.Open();
+
+            //establish a connection
+            //var command = connection.CreateCommand();
+            //this is what tells sql what we want to do
+            //command.CommandText = @"Select * 
+            //                      From Birds";
+            //then run command against connection
+            //execute reader is for when we care about getting all the results of out query
+            //this command only ever holds 1 row of data. when it starts it hold 0 rows of data
+            //var reader = command.ExecuteReader();
+
+            //make place to store each bird
+            //var birds = new List<Bird>();
+
+            //.Read() is a boolean value on each read row.
+            //data readers are weird, only get one row from the results at a time
+            //while (reader.Read()) //"while there is a next record, keep doing this..."
+            //{
+
+            //MAPPING DATA FROM THE RELATIONAL MODEL TO THE OBJECT MODEL (like ORM does for free)
+            //after this explicit example we will use MapFromReader to save effort
+            //var bird = new Bird();
+            //bird.Id = reader.GetGuid(0); //ordinal call works great but you need to know where the column lives. No need to express data types this way.
+            //bird.Size = reader["Size"].ToString(); //if specifically naming the column you want, you then need to convert it to the appropriate data type
+            //Enum.TryParse<BirdType>(reader["Type"].ToString(),out var birdType);
+            //bird.Type = (BirdType)reader["Type"]; //explicit cast when using custom type. if however the types are incorrect and not matching then things blow up
+            //bird.Color = reader["Color"].ToString();
+            //bird.Name = reader["Name"].ToString();
+
+            //var bird = MapFromReader(reader);
+
+            //each bird goes in the list to return later
+            //    birds.Add(bird);
+            //}
+
+            //return birds;
         }
 
         internal Bird Update(Guid id, Bird bird)
         {
-            using var connection = new SqlConnection(_connectionString);
-            connection.Open();
+            // Dapper Way:
+            using var db = new SqlConnection(_connectionString);
+            var sql = @"update Birds 
+                        Set Color = @color,
+                            Name = @name,
+                            Type = @type,
+                            Size = @size
+                        output inserted.*
+                        Where id = @id";
 
-            var cmd = connection.CreateCommand();
-            cmd.CommandText = @"update Birds 
-                                Set Color = @color,
-                                    Name = @name,
-	                                Type = @type,
-	                                Size = @size
-                                output inserted.*
-                                Where id = @id";
+            bird.Id = id;
+            //if we have an object already that maps what we need, we can plug it in.
+            //anonymous data type is not necessary and we can use the bird calls.
+            var updatedBird = db.QuerySingleOrDefault(sql, bird);
+
+            return updatedBird;
+
+            // ADO way:
+            //connection.Open();
+
+            //var cmd = connection.CreateCommand();
+            //cmd.CommandText = @"update Birds
+            //Set Color = @color,
+            //Name = @name,
+            //Type = @type,
+            //Size = @size
+            //output inserted.*
+            //Where Id = @id";
 
             //bird comes from the http request in the controller
-            cmd.Parameters.AddWithValue("Type", bird.Type);
-            cmd.Parameters.AddWithValue("Color", bird.Color);
-            cmd.Parameters.AddWithValue("Size", bird.Size);
-            cmd.Parameters.AddWithValue("Name", bird.Name);
-            cmd.Parameters.AddWithValue("id", id);
+            //note, capitalization dn have to match, just the word needs to match
+            //cmd.Parameters.AddWithValue("Type", bird.Type);
+            //cmd.Parameters.AddWithValue("Color", bird.Color);
+            //cmd.Parameters.AddWithValue("Size", bird.Size);
+            //cmd.Parameters.AddWithValue("Name", bird.Name);
+            //cmd.Parameters.AddWithValue("Id", bird.Id);
 
-            var reader = cmd.ExecuteReader();
+            //execution of the sql against the database
+            //var reader = cmd.ExecuteReader();
 
-            if (reader.Read())
-            {
-                var updatedBird = MapFromReader(reader);
-                return updatedBird;
-            }
+            //working with the results
+            //if (reader.Read())
+            //{
+            //var updatedBird =  MapFromReader(reader);
+            //return updatedBird;
+            //}
+            //return null;
 
-            return null;
         }
 
         internal void Remove(Guid id)
         {
-            using var connection = new SqlConnection(_connectionString);
-            connection.Open();
+            // Dapper way:
+            using var db = new SqlConnection(_connectionString);
 
-            var cmd = connection.CreateCommand();
-            cmd.CommandText = @"Delete 
-                                From Birds 
-                                Where Id = @id";
+            var sql = @"Delete
+                        From Birds 
+                        Where Id = @id";
 
-            cmd.Parameters.AddWithValue("id", id);
+            db.Execute(sql, new { id });
 
-            cmd.ExecuteNonQuery();
+            // ADO way:
+            //connection.Open();
+
+            //var cmd = connection.CreateCommand();
+            //cmd.CommandText = @"Delete
+            //                  From Birds 
+            //                  Where Id = @id";
+
+            //cmd.Parameters.AddWithValue("id", id);
+
+            //cmd.ExecuteNonQuery(); //run the cmd then forget about it
+
         }
 
         internal void Add(Bird newBird)
         {
-            using var connection = new SqlConnection(_connectionString);
-            connection.Open();
+            // Dapper way:
+            using var db = new SqlConnection(_connectionString);
 
-            var cmd = connection.CreateCommand();
-            cmd.CommandText = @"insert into birds(Type,Color,Size,Name)
-                                output inserted.Id
-                                values (@Type,@Color,@Size,@Name)";
+            var sql = @"insert into Birds(Type,Color,Size,Name)
+                                values(@Type, @Color, @Size, @Name)";
 
-            cmd.Parameters.AddWithValue("Type", newBird.Type);
-            cmd.Parameters.AddWithValue("Color", newBird.Color);
-            cmd.Parameters.AddWithValue("Size", newBird.Size);
-            cmd.Parameters.AddWithValue("Name", newBird.Name);
+            //execute scalar, grab the guid data type from the sql, where the newBird properties match the variables (the @ sql parameters)
 
-            //execute the query, but don't care about the results, just number of rows
-            //var numberOfRowsAffected = cmd.ExecuteNonQuery();
-            
+            var id = db.ExecuteScalar<Guid>(sql, newBird);
+            newBird.Id = id;
+
+            // ADO way:
+            // connection.Open(); //opens the connection
+
+            // var cmd = connection.CreateCommand(); //set up a command against the connection
+            // cmd.CommandText = @"insert into Birds(Type,Color,Size,Name)
+            //                 values(@Type, @Color, @Size, @Name)"; //express what command says
+
+            // cmd.Parameters.AddWithValue("Type", newBird.Type); //map values to above @s (avoid sql injection)
+            // cmd.Parameters.AddWithValue("Color", newBird.Color);
+            // cmd.Parameters.AddWithValue("Size", newBird.Size);
+            // cmd.Parameters.AddWithValue("Name", newBird.Name);
+
+            //execute the query, but don't are about the results, just number of rows
+            //var numberOfRowsAffected = cmd.ExecuteNonQuery(); //this cmd.ExecuteNonQuery() returns number of rows affected. bit of a misnomer. essentially it's not returning query results
+
             //execute the query and only get the id of the new row
-            var newId = (Guid) cmd.ExecuteScalar();
+            // var newId = (Guid) cmd.ExecuteScalar();
 
-            newBird.Id = newId;
+            // newBird.Id = newId;
+            //_birds.Add(newBird);
         }
 
         internal Bird GetById(Guid birdId)
         {
-            //connections are like the tunnel between our app and the database
-            using var connection = new SqlConnection(_connectionString);
-            //connections aren't open by default, we've gotta do that ourself
-            connection.Open();
+            // Dapper way:
+            using var db = new SqlConnection(_connectionString);
+            var sql = @"Select * From Birds where id = @id";
+            var bird = db.QuerySingleOrDefault<Bird>(sql, new { id = birdId }); //new { type = property}
+            return bird;
 
-            //this is what tells sql what we want to do
-            var command = connection.CreateCommand();
-            command.CommandText = @"Select *
-                                    From Birds
-                                    where id = @id";
+            // ADO way:
+            //using var connection = new SqlConnection(_connectionString);
+            //connection.Open();
+
+            //var command = connection.CreateCommand();
+            //command.CommandText = @"Select * 
+            //                        From Birds
+            //                        where Id = @id"; //@id is 'parameterization', better than string interpolation because it protects against SQL injection attacks
 
             //parameterization prevents sql injection (little bobby tables)
-            command.Parameters.AddWithValue("id", birdId);
+            //command.Parameters.AddWithValue("id", birdId);
 
             //execute reader is for when we care about getting all the results of our query
-            var reader = command.ExecuteReader();
+            //var reader = command.ExecuteReader();
 
-            if (reader.Read())
-            {
-                return MapFromReader(reader);
-            }
+            //if (reader.Read())
+            //{
+            //    return MapFromReader(reader);
+            //}
 
-            return null;
+            //return default; -- if no match, return null;
+            //return null; //same as above but easier to understand/read
 
             //return _birds.FirstOrDefault(bird => bird.Id == birdId);
         }
 
         Bird MapFromReader(SqlDataReader reader)
         {
-            var bird  = new Bird();
+            var bird = new Bird();
             bird.Id = reader.GetGuid(0);
             bird.Size = reader["Size"].ToString();
             bird.Type = (BirdType)reader["Type"];
---
+++ LinenAndBird/SQL Queries/InsertDelete.sql
@@ -0,0 +1,23 @@
+﻿insert into Birds(Type,Color,Size,Name)
+values(0,'yellow','small','darcy')
+
+update Birds
+Set Color = 'blueberry',
+	Size = 'small',
+	--Name = Name + ' - ' + cast(o.Price as varchar(10)) + 'SOLD'
+	Name = Name + ' - SOLD'
+output inserted.*, deleted.* -- because update does both inserting and deleting
+from birds b
+	/*join orders o 
+		on o.BirdId = b.Id*/
+--Where Color = 'yellow' -- if no where clause is used then all rows Color will be set
+
+--select * from birds;
+--select * from orders;
+
+--update Birds
+--Set Color = @color,
+--	Name = @name,
+--	Type = @type,
+--	Size = @size,
+----Where Id = @id
