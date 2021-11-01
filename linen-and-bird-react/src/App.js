60eb9bd6343e28a3db86ce0b44ac691cd3f67048
BenJarrett <b.cl.jarrett@gmail.com>
2021-11-01 16:02:06 -0500

    added front end ui

--- linen-and-bird-react/src/App.js
+++ linen-and-bird-react/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import BirdList from './components/bird/BirdList';
+import getAllBirds from './helpers/data/birdData';
 
 function App() {
+
+  const [birds, setBirds] = useState([])
+
+  useEffect(() => {getAllBirds()
+    .then(data => setBirds(data))
+  },[]);
+
+  // OR //
+  // useEffect(() => getAllBirds().then(setBirds),[]); //
+
+
   return (
     <div className="App">
-      
+    <BirdList birds={birds}/>
     </div>
   );
 }
---
+++ linen-and-bird-react/src/helpers/data/config.js
@@ -0,0 +1,6 @@
+
+const config = {
+  "baseUrl": "https://localhost:44303"
+}
+
+export default config;
